01b059539c48ed23f0b1ab3f54dd8d807be8fc96
urboypx <guilhermeciotapx@outlook.com>
2022-08-16 01:32:47 -0300

    fix: added schedules list to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Text, FlatList } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import KeyboardAwareScrollView from "../components/KeyboardAwareScrollView";
 
 import Header from "../components/UI/Home/Header";
+
+import SchedulesList from "../components/UI/Home/SchedulesList";
 
 const HomeScreen = () => {
   return (
     <KeyboardAwareScrollView>
       <View style={styles.container}>
         <Header />
+        <SchedulesList />
       </View>
     </KeyboardAwareScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollview: {
-    flexGrow: 1,
-  },
   container: {
     flex: 1,
     alignItems: "center",
