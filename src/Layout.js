8aaf39ca8f655e909f8149ef50b176447dc05091
urboypx <guilhermeciotapx@outlook.com>
2022-08-16 01:31:47 -0300

    fix: status bar

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
+import { StatusBar } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 
 import HomeStack from "./navigation/HomeStack";
 
 const Layout = () => {
+  useLayoutEffect(() => {
+    StatusBar.setBackgroundColor("transparent");
+    StatusBar.setTranslucent(true);
+    StatusBar.setBarStyle("light-content");
+  }, []);
+
   return (
     <NavigationContainer>
       <HomeStack />
