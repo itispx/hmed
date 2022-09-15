8e1a38476434cdf1d7d57cfc0cc68de59f77f494
urboypx <guilhermeciotapx@outlook.com>
2022-09-15 06:20:21 -0300

    feat: next comp logic

--- src/components/UI/Schedules/Next.tsx
+++ src/components/UI/Schedules/Next.tsx
@@ -5,56 +5,74 @@ import { RFValue } from "../../../library/viewport-units";
 
 import useAppSelector from "../../../hooks/useAppSelector";
 
+import { stringToDate } from "../../../library/string-to-date";
+
 import Colors from "../../../constants/Colors";
 
 import Icon from "react-native-vector-icons/FontAwesome5";
 
 const Next: React.FC = () => {
-  const [today] = useState(() => new Date().getDate());
+  const schedule = useAppSelector((state) => {
+    let schedulesArr = state.schedules.schedules
+      .filter((item) => {
+        const currentTime = new Date().toLocaleTimeString();
+        const scheduleTime = stringToDate(item.time).toLocaleTimeString();
 
-  const schedule = {
-    time: "16:00",
-    name: "Doralgina",
-    quantity: 30,
-  };
+        const taken = currentTime < scheduleTime;
+
+        if (taken) {
+          return item;
+        }
+      })
+      .sort((a, b) => {
+        const aConverted = stringToDate(a.time).getTime();
+        const bConverted = stringToDate(b.time).getTime();
+
+        return aConverted - bConverted;
+      });
+
+    return schedulesArr[0];
+  });
 
   return (
-    <View style={{ padding: 15, paddingBottom: 0, backgroundColor: "#FFF" }}>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Text
-          style={{
-            fontSize: RFValue(18),
-            color: Colors.grey,
-            fontWeight: "bold",
-          }}
-        >
-          Próximo remédio{" "}
-        </Text>
-        <Text style={{ fontSize: RFValue(18), color: Colors.grey }}>
-          - {schedule.name} {schedule.quantity}mg
-        </Text>
-      </View>
+    schedule && (
+      <View style={{ padding: 15, paddingBottom: 0, backgroundColor: "#FFF" }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: RFValue(18),
+              color: Colors.grey,
+              fontWeight: "bold",
+            }}
+          >
+            Próximo remédio{" "}
+          </Text>
+          <Text style={{ fontSize: RFValue(18), color: Colors.grey }}>
+            - {schedule.name} {schedule.quantity}mg
+          </Text>
+        </View>
 
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Icon
-          name="clock"
-          solid
-          size={RFValue(40)}
-          color={Colors.primary}
-          style={{ padding: 5 }}
-        />
-        <Text
-          style={{
-            marginBottom: 5,
-            fontSize: RFValue(60),
-            fontWeight: "bold",
-            color: Colors.primary,
-          }}
-        >
-          {schedule.time}
-        </Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Icon
+            name="clock"
+            solid
+            size={RFValue(40)}
+            color={Colors.primary}
+            style={{ padding: 5 }}
+          />
+          <Text
+            style={{
+              marginBottom: 5,
+              fontSize: RFValue(60),
+              fontWeight: "bold",
+              color: Colors.primary,
+            }}
+          >
+            {schedule.time}
+          </Text>
+        </View>
       </View>
-    </View>
+    )
   );
 };
 
