67b8341e1f65150421fa143eb7db1494d1e355c8
urboypx <guilhermeciotapx@outlook.com>
2022-09-15 02:05:36 -0300

    refactor: some renames

---
+++ src/components/UI/Schedules/RenderDay.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import useAppSelector from "../../../hooks/useAppSelector";
+
+import uiActions from "../../../actions/uiActions";
+
+import DayBubble from "../DayBubble";
+
+interface Props {
+  index: number;
+}
+
+const RenderDay: React.FC<Props> = ({ index }) => {
+  const isSelected = useAppSelector(
+    (state) => state.ui.selectedScheduleDay === index
+  );
+
+  function selectBubble(): void {
+    uiActions.selectedScheduleDay(index);
+  }
+
+  return (
+    <DayBubble
+      index={index}
+      isSelected={isSelected}
+      selectBubble={selectBubble}
+    />
+  );
+};
+
+export default RenderDay;
