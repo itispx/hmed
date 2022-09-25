9712e98b5279ee8d5221725d429235989820b5a9
urboypx <guilhermeciotapx@outlook.com>
2022-09-25 19:16:11 -0300

    feat: loading on going to add screen btn

--- src/components/UI/Add/AddIcon.tsx
+++ src/components/UI/Add/AddIcon.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, TouchableHighlight } from "react-native";
 
 import { rfValue } from "../../../library/viewport-units";
@@ -6,17 +6,41 @@ import { rfValue } from "../../../library/viewport-units";
 import Styles from "../../../constants/Styles";
 import Colors from "../../../constants/Colors";
 
+import { showError } from "../../../actions/toastsActions";
+
 import Icon from "react-native-vector-icons/FontAwesome5";
+
+import Loading from "../Loading";
 
 interface Props {
   onPress: () => void;
 }
 
 const AddIcon: React.FC<Props> = ({ onPress }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onPressHandler() {
+    try {
+      setIsLoading(true);
+      onPress();
+      setIsLoading(false);
+    } catch ({ message }) {
+      showError(message as string);
+    }
+  }
+
   return (
-    <TouchableHighlight underlayColor="none" onPress={onPress}>
+    <TouchableHighlight
+      underlayColor="none"
+      disabled={isLoading}
+      onPress={onPressHandler}
+    >
       <View style={[styles.container, Styles.shadow]}>
-        <Icon name="plus" solid size={rfValue(40)} color="#FFF" />
+        {isLoading ? (
+          <Loading color="#FFF" />
+        ) : (
+          <Icon name="plus" solid size={rfValue(40)} color="#FFF" />
+        )}
       </View>
     </TouchableHighlight>
   );
